ee3cb54d4f075c1a2bbe3a3871ecd3ae87951ce0
Joaquin <joacomda@gmail.com>
2022-09-23 16:12:40 +0200

    #Table and Constraints

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -6,6 +6,11 @@ import static javax.persistence.GenerationType.SEQUENCE;
 
 
 @Entity(name = "Student")
+@Table(
+        name = "student",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_email_unique", columnNames = "email")
+        })
 public class Student {
     @Id
     @SequenceGenerator(
@@ -36,8 +41,7 @@ public class Student {
     @Column(
             name = "email",
             nullable = false,
-            columnDefinition = "TEXT",
-            unique = true
+            columnDefinition = "TEXT"
     )
     private String email;
     @Column(name = "age")
