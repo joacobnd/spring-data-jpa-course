ece59a391ce71b241a4e74942ae48b123f7806b7
Joaquin <joacomda@gmail.com>
2022-09-24 13:16:27 +0200

    Native QUeries

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 @SpringBootApplication
@@ -33,18 +34,21 @@ public class Application {
                     "Sanchez",
                     "pedro@gmail.com",
                     40));
-
             studentRepository.saveAll(List.of(maria, maria2, joaquin));
 
+
+            System.out.println("QUERY SOLO");
             studentRepository
-                    .findStudentByEmail("joaco@gmail.com")
+                    .findStudentByEmail("pedro@gmail.com")
                     .ifPresentOrElse(System.out::println, () -> {
                         System.out.println("Student with email not found");
                     });
 
+
+
             studentRepository.findStudentsByFirstNameEqualsAndAgeEquals("Maria", 50).forEach(System.out::println);
             studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThan("Maria", 28).forEach(System.out::println);
-
+            studentRepository.findStudentsByFirstNameEqualsAndAgeIsGreaterThanNative("Maria", 20).forEach(System.out::println);  //Native Query
         };
     }
 
--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -8,10 +8,14 @@ import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    @Query("SELECT s FROM Student WHERE s.email = ?1")
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
-    @Query("SELECT s FROM Student WHERE s.firstName = ?1 AND s.age = ?2")
+    @Query("SELECT s FROM Student s  WHERE s.firstName = ?1 AND s.age = ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(String firstName, Integer age);
-    @Query("SELECT s FROM Student WHERE s.firstName = ?1 AND s.age > ?2")
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age > ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThan(String firstName, Integer age);
+
+    //SQL line
+    @Query(value = "SELECT * FROM student WHERE first_name = ?1 AND age > ?2", nativeQuery = true)
+    List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanNative(String firstName, Integer age);
 }
