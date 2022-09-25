acc5c3de86103fe276049e3d01a9d876daeba331
Joaquin <joacomda@gmail.com>
2022-09-25 18:11:48 +0200

    Sort

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.sql.SQLOutput;
 import java.util.List;
@@ -18,19 +19,31 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository){
         return args -> {
-            Faker faker = new Faker();
-            for (int i = 0; i < 20; i++) {
-                String firstName = faker.name().firstName();
-                String lastName = faker.name().lastName();
-                String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
-                Student student = new Student(
-                        firstName,
-                        lastName,
-                        email,
-                        faker.number().numberBetween(17, 55));
-                studentRepository.save(student);
-            }
+            generateRandomStudents(studentRepository);
+
+            Sort sort = Sort.by(Sort.Direction.ASC, "firstName").and(Sort.by(Sort.Direction.ASC,"age"));
+//            Sort sort = Sort.by("firstName")
+//                    .ascending().and(Sort.by("age").ascending());
+
+            studentRepository.findAll(sort)
+                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+
         };
+    }
+
+    private static void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s.%s@amigoscode.edu", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(17, 55));
+            studentRepository.save(student);
+        }
     }
 
 }
