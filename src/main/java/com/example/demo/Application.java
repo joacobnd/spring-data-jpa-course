c022affe03caa3e628aa3155f5d34e495ecff313
Joaquin <joacomda@gmail.com>
2022-09-25 18:59:01 +0200

    Paging and Sorting, Inspect Paging and Sorting in DebugMode

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.sql.SQLOutput;
@@ -21,14 +23,22 @@ public class Application {
         return args -> {
             generateRandomStudents(studentRepository);
 
-            Sort sort = Sort.by(Sort.Direction.ASC, "firstName").and(Sort.by(Sort.Direction.ASC,"age"));
+            PageRequest pageRequest = PageRequest.of(
+                    0,
+                    5,
+                    Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
+        };
+    }
+
+    private static void extracted(StudentRepository studentRepository) {
+        Sort sort = Sort.by(Sort.Direction.ASC, "firstName").and(Sort.by(Sort.Direction.ASC,"age"));
 //            Sort sort = Sort.by("firstName")
 //                    .ascending().and(Sort.by("age").ascending());
 
-            studentRepository.findAll(sort)
-                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
-
-        };
+        studentRepository.findAll(sort)
+                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
     }
 
     private static void generateRandomStudents(StudentRepository studentRepository) {
